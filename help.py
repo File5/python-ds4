5874e9f7b503a820571e9706e3292caa1765ccd6
Aleksandr Zuev <zuev08@gmail.com>
2021-07-24 17:53:16 +0200

    Align keys, control border printing

--- help.py
+++ help.py
@@ -2,26 +2,27 @@ import re
 
 
 class SpecialKey:
-    def __init__(self, text, name, width):
+    def __init__(self, text, name, width, upper_border=True):
         self.text = text
         self.name = name
         self.width = width
+        self.upper_border = upper_border
 
 
 class AsciiKeyboard:
     BACKSPACE_KEY = SpecialKey('<-', '<Backspace>', 5)
     TAB_KEY = SpecialKey('->|', '<Tab>', 3)
     RETURN_UPPER_KEY = SpecialKey('', '< >', 3)
-    RETURN_KEY = SpecialKey('⏎', '<Return>', 3)
+    RETURN_KEY = SpecialKey('⏎', '<Return>', 6, upper_border=False)
     CAPS_LOCK_KEY = SpecialKey('Caps', '<CapsLock>', 4)
     LSHIFT_KEY = SpecialKey('^    ', '<Shift>', 5)
-    RSHIFT_KEY = SpecialKey('    ^', '<Shift>', 5)
+    RSHIFT_KEY = SpecialKey('        ^', '<Shift>', 9)
 
     CTRL_KEY = SpecialKey('Ctrl', '<Control>', 4)
     OPTION_KEY = SpecialKey('Alt', '<Option>', 3)
     CMD_KEY = SpecialKey('Cmd', '<Command>', 3)
 
-    SPACE_KEY = SpecialKey(' ', '<Space>', 24)
+    SPACE_KEY = SpecialKey(' ', '<Space>', 26)
 
     PADDING = 1
 
@@ -30,7 +31,7 @@ class AsciiKeyboard:
         self._keys = [
             ['§'] + [str(i) for i in range(1, 10)] + ['0', '-', '=', self.BACKSPACE_KEY],
             [self.TAB_KEY, 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', self.RETURN_UPPER_KEY],
-            [self.CAPS_LOCK_KEY, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '\\', self.RETURN_KEY],
+            [self.CAPS_LOCK_KEY, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', self.RETURN_KEY],
             [self.LSHIFT_KEY, 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', self.RSHIFT_KEY],
             [self.CTRL_KEY, self.OPTION_KEY, self.CMD_KEY, self.SPACE_KEY, self.CMD_KEY, self.OPTION_KEY]
         ]
@@ -38,7 +39,7 @@ class AsciiKeyboard:
     def __str__(self):
         result_rows = []
 
-        def upper_part(row):
+        def upper_part(row, first=False, last=False):
             result_row = ''
 
             # upper part of the keys row
@@ -48,19 +49,36 @@ class AsciiKeyboard:
                 else:
                     width = 1
                 width += 2 * self.PADDING
-                result_row += ',' + '-' * width
-            result_row += ','
+
+                delim = '-'
+                if isinstance(key, SpecialKey) and not key.upper_border:
+                    delim = ' '
+
+                result_row += ',' + delim * width
+
+            if first:
+                result_row += ','
+            elif last:
+                result_row += '\''
+            else:
+                result_row += '|'
             
             # take into account previous upper part
             if len(result_rows) >= 2:
                 prev_upper_row = result_rows[-2]
                 delim_indexes = {m.start() for m in re.finditer(',', prev_upper_row)}
                 updated_result_row = ''
-                for i, c in enumerate(result_row):
+                for i, c in enumerate(result_row[:-1]):
                     if i in delim_indexes:
                         updated_result_row += '\''
                     else:
                         updated_result_row += c
+                if first:
+                    updated_result_row += ','
+                elif last:
+                    updated_result_row += '\''
+                else:
+                    updated_result_row += '|'
                 result_row = updated_result_row
 
             result_rows.append(result_row)
@@ -82,11 +100,11 @@ class AsciiKeyboard:
             result_row += '|'
             result_rows.append(result_row)
         
-        for row in self._keys:
-            upper_part(row)
+        for i, row in enumerate(self._keys):
+            upper_part(row, first=i == 0)
             lower_part(row)
 
-        upper_part(row)
+        upper_part(row, last=True)
         return '\n'.join(result_rows)
 
 
